b967a5293dceb1041f9a671c506f576ef606d4b6
nikhil tk <nikhiltk02@gmail.com>
2022-08-05 11:53:38 +0530

    added header,recipies,shoing-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { RecipiesListComponent } from './recipies/recipies-list/recipies-list.component';
+import { RecipiesDetailsComponent } from './recipies/recipies-details/recipies-details.component';
+import { RecipieItemComponent } from './recipies/recipies-list/recipie-item/recipie-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipiesComponent,
+    RecipiesListComponent,
+    RecipiesDetailsComponent,
+    RecipieItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipies/recipies-details/recipies-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipies-details',
+  templateUrl: './recipies-details.component.html',
+  styleUrls: ['./recipies-details.component.css']
+})
+export class RecipiesDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
